e5e9e67009ccb5857328ce05d8cedcceb63d94dc
ViliusKk <vilius.kuokstas@gmail.com>
2024-11-24 13:11:55 +0200

    UI changes

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -1,15 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicPlayer : MonoBehaviour
 {
+    public TMP_Text musicName;
+    public TMP_Text currentTime;
+    public TMP_Text musicLength;
+    public Slider slider;
     AudioSource audioSource;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        musicName.text = audioSource.clip.name;
+        int lengthInSeconds = Convert.ToInt32(audioSource.clip.length);
+        slider.maxValue = lengthInSeconds;
+        musicLength.text = $"{lengthInSeconds / 60}:{lengthInSeconds-(60* (lengthInSeconds / 60))}";
+    }
+
+    void Update()
+    {
+        currentTime.text = $"{Convert.ToInt32(audioSource.time) / 60}:{Convert.ToInt32(audioSource.time) - (60 * (Convert.ToInt32(audioSource.time) / 60))}";
     }
 
     public void Pause()
@@ -22,6 +37,16 @@ public class MusicPlayer : MonoBehaviour
         {
             audioSource.UnPause();
         }
+    }
+
+    public void Next()
+    {
+
+    }
+
+    public void Previous()
+    {
+
     }
 
     public void SetTime(float time)
