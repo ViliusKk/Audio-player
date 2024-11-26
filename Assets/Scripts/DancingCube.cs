d7c918084a6123eaefcee2f4d2656f4c4be959b6
ViliusKk <vilius.kuokstas@gmail.com>
2024-11-26 13:45:10 +0200

    Dancing cubes

---
+++ Assets/Scripts/DancingCube.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DancingCube : MonoBehaviour
+{
+    public float sensitivity = 5;
+
+    public void Dance(float volume)
+    {
+        //transform.localScale = Vector3.one * volume * sensitivity; // changes xyz
+        transform.localScale = new Vector3(1, 1 * volume * sensitivity, 1 * volume * sensitivity); // changes yz
+        //transform.localScale = new Vector3(1, 1 * volume * sensitivity, 1); // changes y
+    }
+}
